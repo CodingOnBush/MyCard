4873fe507491832b3317014dc45f89ddbe16b168
Mostafa <mostafa.omrane@proton.me>
2023-04-01 14:11:30 +0200

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by VegaPunk on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
